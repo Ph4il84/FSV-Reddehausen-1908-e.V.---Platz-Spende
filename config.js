66af93f9becc4704343ce1808a551521d67bda2c
Ph4il84 <phildietrich@web.de>
2026-09-20 19:55:37 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // Der "anon public key" ist bewusst öffentlich sichtbar -- Zugriffsrechte
 // werden ausschließlich über die Row-Level-Security-Regeln in schema.sql geregelt.
 
-const SUPABASE_URL = "https://sb_publishable_OkEkaIPgb-p0ECfDtnsmFQ_RJ69ThYY.supabase.co";
+const SUPABASE_URL = "https://saeywgqgxhsitdwdtrxp.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNhZXl3Z3FneGhzaXRkd2R0cnhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk5MDIxNjEsImV4cCI6MjEwNTQ3ODE2MX0.rjcPHnEd2GzHWM54xfZU5DDa1ca4i1cPH0GhhHFBASc";
 
 // EmailJS: Benachrichtigung an den Verein bei jeder neuen Reservierung.
